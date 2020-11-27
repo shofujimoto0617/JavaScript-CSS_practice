3c36387ce964c02fcc0ea35da3a41981a01b57b4
shofujimoto0617 <shofujimoto017@gmail.com>
2020-11-27 21:47:03 +0900

    sample_part4

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hero = new HeroSlider('.swiper-container');
     hero.start();
+
+    const cb = function (el, isIntersecting) {
+        if(isIntersecting) {
+            const ta = new TweenTextAnimation(el);
+            el.classList.add();
+        }
+    } 　
+
+    const so = new ScrollObserver('.tween-animate-title', cb);
 });
 
