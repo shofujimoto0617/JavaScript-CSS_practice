e003fb3b70c8217299f6ebe3c645f9cc9f34bbb4
shofujimoto0617 <shofujimoto017@gmail.com>
2020-11-27 22:26:56 +0900

    sample_part5

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -10,5 +10,15 @@ document.addEventListener('DOMContentLoaded', function () {
     } 　
 
     const so = new ScrollObserver('.tween-animate-title', cb);
+
+    const _inviewAnimation = function(el, inview) {
+        if(inview) {
+            el.classList.add('inview');
+        }else {
+            el.classList.remove('inview');
+        }
+    }
+
+    const so2 = new ScrollObserver('.cover-slide', __inviewAnimation);
 });
 
