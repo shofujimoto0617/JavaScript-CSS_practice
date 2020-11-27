b9ff70bebdb398d3ec5f19ab59842211c5f1fdf6
shofujimoto0617 <shofujimoto017@gmail.com>
2020-11-27 13:52:35 +0900

    Finish:chapter05_070_part4

--- 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
+++ 05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start/main.js
@@ -1,6 +1,9 @@
-let mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
-    effect: 'coverflow',
-  })
+document.addEventListener('DOMContentLoaded', function() {
+    const hero = new HeroSlider('.swiper-container');
+    hero.start();
+
+    // setTimeout(() => {
+    //   hero.stop();
+    // },5000);
+});
+
