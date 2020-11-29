ed5b408836c181cb8baa317310e3d2b912b7eca2
shofujimoto0617 <shofujimoto017@gmail.com>
2020-11-29 17:49:37 +0900

    Finish:sample

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -1,24 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {    
+document.addEventListener('DOMContentLoaded', function () {
     const main = new Main();
 });
 
 class Main {
     constructor() {
         this.header = document.querySelector('.header');
+        this.sides = document.querySelectorAll('.side');
         this._observers = [];
         this._init();
+    }
+
+    set observers(val) {
+        this._observers.push(val);
+    }
+
+    get observers() {
+        return this._observers;
     }
 
     _init() {
         new MobileMenu();
         this.hero = new HeroSlider('.swiper-container');
+        Pace.on('done', this._paceDone.bind(this));
+    }
+
+    _paceDone() {
         this._scrollInit();
     }
 
     _inviewAnimation(el, inview) {
         if(inview) {
             el.classList.add('inview');
-        } else {
+        }else {
             el.classList.remove('inview');
         }
     }
@@ -28,6 +41,14 @@ class Main {
             this.header.classList.remove('triggered');
         } else {
             this.header.classList.add('triggered');
+        }
+    }
+
+    _sideAnimation(el, inview) {
+        if(inview) {
+            this.sides.forEach(side => side.classList.add('inview'));
+        } else {
+            this.sides.forEach(side => side.classList.remove('inview'));
         }
     }
 
@@ -46,15 +67,23 @@ class Main {
         }
     }
 
+    _destroyObservers() {
+        this.observers.forEach(ob => {
+            ob.destroy();
+        });
+    }
+
+    destroy() {
+        this._destroyObservers();
+    }
+
     _scrollInit() {
-        this._observers.push(
-            new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false})
-        );
-        this._observers.push(
-            new ScrollObserver('.cover-slide', this._inviewAnimation)       
-        );
-        new ScrollObserver('.tween-animate-title', this._textAnimation);
-        new ScrollObserver('.swiper-container', this._toggleSlideAnimation.bind(this), {once: false});
+        this.observers = new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false});
+        this.observers = new ScrollObserver('.cover-slide', this._inviewAnimation);
+        this.observers = new ScrollObserver('.appear', this._inviewAnimation);
+        this.observers = new ScrollObserver('.tween-animate-title', this._textAnimation, {rootMargin: "-200px 0px"});
+        this.observers = new ScrollObserver('.swiper-container', this._toggleSlideAnimation.bind(this), {once: false});
+        this.observers = new ScrollObserver('#main-content', this._sideAnimation.bind(this), {once: false, rootMargin: "-300px 0px"});
     }
 }
 
